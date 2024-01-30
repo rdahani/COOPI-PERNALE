2a0ba52a48d099aec8349677496a07ba1fb1c682
rdahani <rdahani3@gmail.com>
2024-01-30 16:54:14 +0100

    update

---
+++ app/Models/Settings/Mois.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Settings;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mois extends Model
+{
+    use HasFactory;
+}
